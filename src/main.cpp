6201d7fbe96d8e2df5c5f22b18e6f8d84f770945
Homuncoli <dan18765@spengergasse.at>
2021-03-11 16:16:10 +0100

    Add spdlog example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+
 #include "Forumla.h"
 
 int main() {
     std::cout << Formula::square(10) << '\n';
+
+    spdlog::info("Sample Info output.");
+    spdlog::warn("Sample Warn output.");
+    spdlog::error("Sample Error output.");
+
     return 0;
 }
