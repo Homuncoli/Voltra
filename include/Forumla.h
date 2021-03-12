a6f59f58c92f93282a0b031203b2d709f597ca16
Homuncoli <dan18765@spengergasse.at>
2021-03-12 17:23:16 +0100

    Add example doc

--- include/Forumla.h
+++ include/Forumla.h
@@ -1,7 +1,17 @@
 #ifndef FORMULA_H
 #define FORMULA_H
 
+/**
+ * @brief Defines the math functions
+ * 
+ */
 namespace Formula {
+    /**
+     * @brief Squares a number
+     * 
+     * @param number number to square
+     * @return int square of number
+     */
     int square(double number);
 }
 
