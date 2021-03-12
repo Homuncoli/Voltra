0ad2d3b502b662f562476fadfe5269edfdb4aa8d
Homuncoli <43949214+Homuncoli@users.noreply.github.com>
2021-03-12 18:58:49 +0100

    feature/06-doxygen (#10)

---
+++ include/Forumla.h
@@ -0,0 +1,18 @@
+#ifndef FORMULA_H
+#define FORMULA_H
+
+/**
+ * @brief Defines the math functions
+ * 
+ */
+namespace Formula {
+    /**
+     * @brief Squares a number
+     * 
+     * @param number number to square
+     * @return int square of number
+     */
+    int square(double number);
+}
+
+#endif
--- src/Forumla.h
+++
@@ -1,8 +0,0 @@
-#ifndef FORMULA_H
-#define FORMULA_H
-
-namespace Formula {
-    int square(double number);
-}
-
-#endif
